7dad04b0b2efd7e29ec4eb4a1c6adbbc0a7009bd
Thestias <franceschiconrado@gmail.com>
2021-01-27 11:35:17 -0300

    Register new path, added custom form to login view

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .forms import CustomAuthenticationForm
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/register_login.html', extra_context={'action': 'Login'}), name='login'),
+    path('', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', authentication_form=CustomAuthenticationForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='home.html'), name='logout'),
 ]

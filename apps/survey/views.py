1be250684ee35b1a8533dc4950027a2f4ad97b41
Thestias <franceschiconrado@gmail.com>
2021-02-07 00:48:29 -0300

    Added http methods allowed

--- apps/survey/views.py
+++ apps/survey/views.py
@@ -3,11 +3,13 @@ from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Survey, Option, Question
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 from django.contrib import messages
 
 # Create your views here.
 
 
+@require_http_methods(['GET'])
 @login_required(login_url='/login')
 def create_survey(request, survey_id):
     survey_instance = get_object_or_404(Survey, id=survey_id)
@@ -23,6 +25,7 @@ def create_survey(request, survey_id):
         context={'question_formset': question_formset, 'survey_id': survey_instance.id})
 
 
+@require_http_methods(['POST'])
 @login_required(login_url='/login')
 def add_survey(request, survey_id):
     survey_instance = get_object_or_404(Survey, id=survey_id)
@@ -30,14 +33,9 @@ def add_survey(request, survey_id):
     if survey_instance.author.id != request.user.id:  # Checking ownership
         return JsonResponse({'Error': 'You are not the owner of this survey!'})
 
-    if request.method == 'POST':
-        survey_instance = get_object_or_404(Survey, id=survey_id)
-        question_formset = QuestionFormSet(request.POST, request.FILES, instance=survey_instance)
-
-        try:
-            if question_formset.is_valid():
-                question_formset.save()
-                return JsonResponse({'Success': 'Survey Created Succesfully!'})
-        except Exception as e:
-            print(e)
-            return JsonResponse({'Error': e})
+    question_formset = QuestionFormSet(request.POST, instance=survey_instance)
+    if question_formset.is_valid():
+        question_formset.save()
+        return JsonResponse({'Success': 'Survey updated/created succesfully'})
+    else:
+        return JsonResponse({'Error': 'Error while creating survey', 'Errors': question_formset.errors})

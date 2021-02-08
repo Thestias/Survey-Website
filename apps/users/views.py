f42194dd5a12b1dd9f2df20bdd83c9da4b746950
Thestias <franceschiconrado@gmail.com>
2021-02-07 22:39:29 -0300

    Users can create Surveys from Profile

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreation
 from ..survey.models import Survey, Submission
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
@@ -15,7 +18,17 @@ def register(request):
     return render(request, 'users/register.html', context={'form': register_form})
 
 
+@login_required
 def profile(request):
     user_surveys = Survey.objects.filter(author=request.user.id)
+    if request.method == 'POST':
+        if 'add-survey' in request.POST:
+            try:
+                survey = Survey(author=request.user)
+            except Exception as e:
+                messages.error(request, e)
+            else:
+                survey.save()
+                messages.success(request, 'Survey Created!')
 
     return render(request, 'users/profile.html', context={'user_surveys': user_surveys})

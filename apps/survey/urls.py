4960f3d92aa5c73469f2cc3fdc9afe3856a68ce9
Thestias <franceschiconrado@gmail.com>
2021-02-10 03:41:46 -0300

    Added start_survey view and improved URL paths
    	templates/survey/survey.html  Fixed the name of the URL that redirects to the answering of the survey
    	templates/survey/survey_start.html Initial Commit
    	apps/survey/urls.py Improved the URL Paths (again)

--- apps/survey/urls.py
+++ apps/survey/urls.py
@@ -7,13 +7,13 @@ urlpatterns = [
     path('<survey_id>/', views.survey, name='survey'),
 
     # Displays the questions and their options
-    #path('<survey_id>', views.start_survey, name='start_survey'),
+    path('<survey_id>', views.start_survey, name='start_survey'),
     #
-    path('edit/<survey_id>/', views.edit_questions, name='edit_questions'),
+    path('<survey_id>/edit/', views.edit_questions, name='edit_questions'),
 
     # BELOW - PATHS FOR AJAX REQUESTS
     # This one recieves AJAX POST requests to add/edit surveys
-    path('edit/<survey_id>/submit/', views.ajax_edit_questions, name='ajax_edit_questions'),
+    path('<survey_id>/edit/submit/', views.ajax_edit_questions, name='ajax_edit_questions'),
 
     # When a User SUBMITS its answers
     #path('<survey_id>/submit', views.submit_survey, name='submit_survey')

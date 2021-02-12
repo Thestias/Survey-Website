0cbf2d86cf3d5a5c8ba371b94567b112aaca7e05
Thestias <franceschiconrado@gmail.com>
2021-02-12 08:54:19 -0300

    Added Survey models to admin page

--- apps/survey/admin.py
+++ apps/survey/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Survey, Submission, Answer, Option
 # Register your models here.
+
+admin.site.register(Survey)
+admin.site.register(Submission)
+admin.site.register(Answer)
+admin.site.register(Option)

0e3dd73ac660b959431ee786f53e3d5cf4934c27
Thestias <franceschiconrado@gmail.com>
2021-02-12 08:54:34 -0300

    Added better dunder str methods

--- apps/survey/models.py
+++ apps/survey/models.py
@@ -14,7 +14,7 @@ class Survey(models.Model):
     is_public = models.BooleanField(default=True)
 
     def __str__(self):
-        return f'Survey Title: {self.title} - Author_ID: {self.author} - Is active?: {self.is_active} - Is Public?: {self.is_public}'
+        return self.title
 
 
 class Question(models.Model):
@@ -22,7 +22,7 @@ class Question(models.Model):
     question = models.CharField(max_length=500)
 
     def __str__(self):
-        return f'From the Survey_ID: {self.survey} - Question: {self.question}'
+        return self.question
 
 
 class Option(models.Model):
@@ -37,8 +37,14 @@ class Submission(models.Model):
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return 'Submission for: ' + str(self.survey)
+
 
 class Answer(models.Model):
     submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     option = models.ForeignKey(Option, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Question: ' + str(self.question) + ' - Answer: ' + str(self.option)

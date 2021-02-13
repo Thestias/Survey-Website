678bc93c6215f5e180c55bf0d61bf27d234e7f7f
Thestias <franceschiconrado@gmail.com>
2021-02-13 14:14:40 -0300

    Added common SetUp and tests for Survey view

--- apps/survey/tests/test_views.py
+++ apps/survey/tests/test_views.py
@@ -6,13 +6,18 @@ import json
 from django.http import HttpResponseRedirect, HttpResponse
 
 
-class TestSurvey_ajax_edit_questions_view(TestCase):
+class CommonSetUp(TestCase):
     def setUp(self):
         User.objects.create(username='UserTest123', password='test1234')
         User.objects.create(username='UserTestSecond', password='test11111')
         Survey.objects.create(
             author=User.objects.get(id=1),
             title='Title Test', description='Test Test Test')
+
+
+class TestSurvey_ajax_edit_questions_view(CommonSetUp, TestCase):
+    def setUp(self):
+        super().setUp()
         self.data = {
             'question_set-TOTAL_FORMS': ['1'],
             'question_set-INITIAL_FORMS': ['0'],
@@ -72,13 +77,9 @@ class TestSurvey_ajax_edit_questions_view(TestCase):
         self.assertEqual('Error while creating survey', json.loads(response.content)['Error'])
 
 
-class TestSurvey_edit_questions_view(TestCase):
+class TestSurvey_edit_questions_view(CommonSetUp, TestCase):
     def setUp(self):
-        User.objects.create(username='UserTest123', password='test1234')
-        User.objects.create(username='UserTestSecond', password='test11111')
-        Survey.objects.create(
-            author=User.objects.get(id=1),
-            title='Title Test', description='Test Test Test')
+        super().setUp()
 
     def test_Survey_doesnt_exist(self):
         '''
@@ -111,3 +112,42 @@ class TestSurvey_edit_questions_view(TestCase):
 
         self.assertEqual(200, response.status_code)
         self.assertIsInstance(response, HttpResponse)
+
+
+class TestSurvey_survey_view(CommonSetUp, TestCase):
+    def setUp(self):
+        super().setUp()
+        self.client.force_login(User.objects.get(id=1))
+
+    def test_Survey_doesnt_exist(self):
+        '''
+        Checks that the edit_questions view returns a 404 Status code when a Survey that doesnt exist is requested
+        '''
+        response = self.client.get(reverse('survey', args=['1212']))
+
+        self.assertEqual(404, response.status_code)
+
+    def test_Survey_delete(self):
+        response = self.client.post(reverse('survey', args=['1']), data={'delete': '1'})
+
+        self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertEqual(response.url, '/profile/')
+
+    def test_Survey_edit_valid(self):
+        data = {'title': ['test'], 'description': ['testtest'], 'is_active': ['on']}
+
+        response = self.client.post(reverse('survey', args=['1']), data=data)
+        message = list(response.context.get('messages'))[0]
+        self.assertEqual(message.tags, "success")
+        self.assertTrue("Survey updated!" in message.message)
+
+    def test_Survey_edit_invalid(self):
+        data = {'is_active': ['on']}
+        response = self.client.post(reverse('survey', args=['1']), data=data)
+        message = list(response.context.get('messages'))[0]
+        self.assertEqual(message.tags, "error")
+        self.assertTrue("Invalid form!" in message.message)
+
+    def test_not_the_owner(self):
+        response = self.client.get(reverse('survey', args=['1']))
+        self.assertTrue('survey_edit_form' in response.context)

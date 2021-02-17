3cc4e8762b0698423605c59d3eac2c65b22ca66e
Thestias <franceschiconrado@gmail.com>
2021-02-17 18:52:47 -0300

    Setup WhiteNoise

--- SurveyWebsite/settings/prod.py
+++ SurveyWebsite/settings/prod.py
@@ -4,9 +4,17 @@ from dotenv import load_dotenv
 # Loading Dot-Enviroment Variables
 dotenv_path = os.path.join(os.path.dirname(BASE_DIR), '.env')
 load_dotenv(dotenv_path)
+
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
+
+
+# Serving Static Files
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')  # Adding WhiteNoise after the SecurityMiddleware
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')

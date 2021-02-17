571233bd0b557b9c13364daa928295d294f51448
Thestias <franceschiconrado@gmail.com>
2021-02-17 19:30:33 -0300

    Fixed settings path

--- SurveyWebsite/wsgi.py
+++ SurveyWebsite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SurveyWebsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SurveyWebsite.settings.prod')
 
 application = get_wsgi_application()

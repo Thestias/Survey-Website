4c1ed30bc030311c54fdbb23a7604334bb63e9a7
Thestias <franceschiconrado@gmail.com>
2021-04-13 15:57:10 -0300

    Preped Prod DB for Postgres}

--- SurveyWebsite/settings/prod.py
+++ SurveyWebsite/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 from dotenv import load_dotenv
 
 # Loading Dot-Enviroment Variables
@@ -10,7 +11,7 @@ load_dotenv(dotenv_path)
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'whispering-island-88200.herokuapp.com/']
 
 
 # Serving Static Files
@@ -18,3 +19,8 @@ ALLOWED_HOSTS = ['127.0.0.1']
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')  # Adding WhiteNoise after the SecurityMiddleware
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
